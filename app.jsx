721a71c2250a3035e92c2d2bd6cdae383218ab71
Bryan Maamo <ninbryan@users.noreply.github.com>
2025-05-24 13:26:54 -0700

    react vite

---
+++ app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.body);
+root.render(<App />);
+
+function App() {
+  return (
+    <div className="banana">
+      <h1>React me</h1>
+    </div>
+  )
+}
